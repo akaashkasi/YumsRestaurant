63143d8ead9ccc654c5a568b530acf3bc00e0127
akaashkasi <akaashkasi@gmail.com>
2023-12-19 21:12:59 -0600

    Added necessary components to create proper framework for frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.js
+// Main component that renders all other components
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import MenuPreview from './components/MenuPreview';
+//import Recommendations from './components/Recommendations';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/about" component={About} />
+          <Route path="/menu" component={MenuPreview} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/" element={<Hero />} />
+        </Routes>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+// components/Footer.jsx
+import '../components/css/Footer.css'; // Make sure to create a corresponding CSS file for styling
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa'; // Importing icons
+
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <section className="footer-section">
+          <h4>About Yum&apos;s</h4>
+          <p>Discover the story of our restaurant and our dedication to Chinese cuisine.</p>
+        </section>
+        <section className="footer-section">
+          <h4>Contact</h4>
+          <p>123 Yum St, Collierville, TN 38017</p>
+          <p>(901) 123-4567</p>
+        </section>
+        <section className="footer-section">
+          <h4>Follow Us</h4>
+          {/* Icons from React Icons can be used here */}
+          <div className="social-icons">
+          <FaFacebookF />
+          <FaTwitter />
+          <FaInstagram />
+        </div>
+
+        </section>
+        <section className="footer-section">
+          <h4>Newsletter</h4>
+          <form>
+            <input type="email" placeholder="Your email address" />
+            <button type="submit">Subscribe</button>
+          </form>
+        </section>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; {new Date().getFullYear()} Yum&apos;s Restaurant. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
