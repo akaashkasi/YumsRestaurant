9ca0aa24c77833503f0dba911275cdb414d91849
Brandon Zhang <brandonzhang0223@gmail.com>
2023-12-21 22:48:57 -0600

    updated serverside code to connect to mongo and attempt to query

---
+++ src/server/api/v1/menuModel.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const menuItemSchema = new mongoose.Schema({
+    id: Number,
+    name: String,
+    price: Number,
+    size: mongoose.Schema.Types.Mixed
+});
+
+const categorySchema = new mongoose.Schema({
+    categoryName: String,
+    items: [menuItemSchema]
+});
+
+const menuSchema = new mongoose.Schema({
+    menuType: String,
+    categories: [categorySchema]
+}, { collection: 'menu-items' });
+
+const Menu = mongoose.model('Menu', menuSchema);
+
+export default Menu;
---
+++ src/server/api/v1/mongoQuery.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import connectDB from "./mongodb.js"; // Adjust the path
+import Menu from "./menuModel.js"; // Adjust the path
+
+dotenv.config();
+
+const queryMenus = async () => {
+  try {
+    await connectDB(); // Connect to the database
+
+    const allMenus = await Menu.find();
+    console.log("All menus:", allMenus);
+
+    // Query for the Chinese menu
+    const chineseMenu = await Menu.findOne({ menuType: "Chinese" });
+    console.log("Chinese Menu:", JSON.stringify(chineseMenu, null, 2));
+
+    // Query for the American menu
+    const americanMenu = await Menu.findOne({ menuType: "American" });
+    console.log("American Menu:", JSON.stringify(americanMenu, null, 2));
+  } catch (error) {
+    console.error("Error:", error);
+  } finally {
+    mongoose.disconnect();
+  }
+};
+
+export default queryMenus;
