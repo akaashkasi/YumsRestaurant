0fc245f58193835703aad1102eb79a5262f7b21e
Brandon Zhang <brandonzhang0223@gmail.com>
2023-12-22 00:28:57 -0600

    Added menu functionality to populate screen with different categories and also added loading spinner fof when page is loading

---
+++ src/client/components/LoadingSpinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/LoadingSpinner.css'; // Make sure to create a corresponding CSS file
+
+const LoadingSpinner = () => (
+  <div className="spinner-container">
+    <div className="loading-spinner"></div>
+  </div>
+);
+
+export default LoadingSpinner;
