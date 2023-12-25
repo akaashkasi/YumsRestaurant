361084611cfa40cbf0b30d7bcce447600dfc3c58
Brandon Zhang <brandonzhang0223@gmail.com>
2023-12-25 15:56:18 -0600

    missed one

--- src/server/api/v1/mongoQuery.js
+++ src/server/api/v1/mongoQuery.js
@@ -3,7 +3,6 @@ import Menu from './menuModel.js'; // Adjust the path
 const queryMenus = async () => {
   try {
     const allMenus = await Menu.find();
-    console.log('Menus retrieved:', allMenus);
     return allMenus;
   } catch (error) {
     console.error('Error:', error);
