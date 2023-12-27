832020b4ecb385f35939dfe56f0272be80bf5ae8
akaashkasi <akaashkasi@gmail.com>
2023-12-26 23:54:03 -0600

    Added logic to not to be able to get to checkout without items in the cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect, useState, useContext } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+} from 'react-router-dom';
 import Header from './client/components/Header';
 import Hero from './client/components/Hero';
 import About from './client/components/About';
@@ -8,17 +13,37 @@ import Contact from './client/components/Contact';
 import Location from './client/components/Location';
 import Footer from './client/components/Footer';
 import Cart from './client/components/Cart';
-import LoadingSpinner from './client/components/LoadingSpinner'; // Import the spinner component
+import LoadingSpinner from './client/components/LoadingSpinner';
 import Checkout from './client/components/Checkout';
-import { CartProvider } from './client/context/CartContext';
+import { CartProvider, CartContext } from './client/context/CartContext';
+import PropTypes from 'prop-types';
 import './App.css';
 
+function CheckoutGuard({ children }) {
+  const { cartItems, setShowPopup } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (cartItems.length === 0) {
+      setShowPopup(true);
+      navigate('/menu'); // Redirect to the home page or any other page
+    }
+  }, [cartItems, setShowPopup, navigate]);
+
+  if (cartItems.length === 0) {
+    return null;
+  }
+
+  return children;
+}
+
 function App() {
-  const [isLoading, setIsLoading] = useState(false); // State to manage loading
+  const [isLoading, setIsLoading] = useState(false);
   const [cartItems, setCartItems] = useState(() => {
     const savedCart = localStorage.getItem('cart');
     return savedCart ? JSON.parse(savedCart) : [];
-  }); // State to manage cart items
+  });
+  const [showPopup, setShowPopup] = useState(false);
 
   const addToCart = item => {
     setIsLoading(true);
@@ -31,50 +56,59 @@ function App() {
     setCartItems(prevItems => prevItems.filter(item => item.id !== itemId));
     setIsLoading(false);
   };
-  const handleCheckout = () => {
-    console.log('Proceeding to checkout', cartItems);
-    // Add your logic to navigate to the checkout page or handle checkout
+
+  const onCheckout = () => {
+    // Logic for checkout, you can customize this as needed
+    console.log('Checkout initiated');
   };
 
   useEffect(() => {
     localStorage.setItem('cart', JSON.stringify(cartItems));
   }, [cartItems]);
 
+  const cartContextValue = {
+    cartItems,
+    addToCart,
+    removeFromCart,
+    setShowPopup,
+  };
+
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <CartProvider value={{ cartItems, addToCart, removeFromCart }}>
+    <CartProvider value={cartContextValue}>
+      <Router>
+        <div className="App">
+          <Header />
+
           {isLoading && <LoadingSpinner message="Loading cart..." />}
+          {showPopup}
           <Routes>
             <Route path="/about" element={<About />} />
             <Route
               path="/menu"
-              element={<MenuPreview onCheckout={handleCheckout} />}
+              element={<MenuPreview onCheckout={onCheckout} />}
             />
             <Route path="/contact" element={<Contact />} />
             <Route path="/location" element={<Location />} />
             <Route path="/" element={<Hero />} />
-            <Route
-              path="/cart"
-              element={
-                <Cart
-                  cartItems={cartItems}
-                  onRemoveFromCart={removeFromCart}
-                  onCheckout={() => console.log('Checkout', cartItems)} // Replace with your checkout logic
-                />
-              }
-            />
+            <Route path="/cart" element={<Cart onCheckout={onCheckout} />} />
             <Route
               path="/checkout"
-              element={<Checkout cartItems={cartItems} />}
+              element={
+                <CheckoutGuard>
+                  <Checkout />
+                </CheckoutGuard>
+              }
             />
           </Routes>
-        </CartProvider>
-        <Footer />
-      </div>
-    </Router>
+
+          <Footer />
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
+CheckoutGuard.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 export default App;
