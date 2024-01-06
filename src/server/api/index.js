2d44d9162fb08a3197b3d5b2e480cb9c1712b67c
akaashkasi <akaashkasi@gmail.com>
2024-01-05 23:26:34 -0600

    Added fixes to make deploying nice

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import path from 'path';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
 import emailHandler from './v1/emailHandler.js';
 import process from 'process';
 import menuHandler from './v1/menuHandler.js';
 import connectDB from './v1/mongodb.js';
+
+// Define __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
@@ -14,6 +20,14 @@ connectDB()
     console.log('MongoDB connected');
     emailHandler(app); // Register email handler routes
     menuHandler(app); // Register menu handler routes
+
+    // Serve static files from the React build directory
+    app.use(express.static(path.join(__dirname, '../../../dist')));
+
+    // Catch-all route handler to serve index.html
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../../../dist', 'index.html'));
+    });
 
     const PORT = process.env.PORT || 5174;
     app.listen(PORT, () => {
